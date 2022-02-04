17c9832ea85b7bcf528c815fb9624a0c2561b1aa
Jorge Hurtado <jjhurtado796@gmail.com>
2022-02-03 23:28:14 -0500

    ADD location Model

---
+++ lib/core/location/app/models/location_model.dart
@@ -0,0 +1,13 @@
+class location_model {
+
+  String name;
+  String url;//ip+port
+  int data_base_id;
+  int user_id;
+  String user_name;
+  String user_password;
+
+  location_model(
+       this.name, this.url, this.data_base_id, this.user_id,
+      this.user_name, this.user_password);
+}
