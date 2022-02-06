9df656ecdcaf6d7cdcfacc06bc43afa664b4fe0d
Cristhian Damian Rodriguez Aguila <cristhiandamianrodriguez@gmail.com>
2022-02-06 11:26:26 -0500

    No hice nada
    Al final no tuve que cambiar nada

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,6 @@ class Home extends StatelessWidget {
 class Other extends StatelessWidget {
   // You can ask Get to find a Controller that is being used by another page and redirect you to it.
   final main_controller c = Get.find();
-
   @override
   Widget build(context) {
     // Access the updated count variable
