712d4f87e8ece16ac6be53a6b4c5d3d59b3bf7af
Jorge Hurtado <jjhurtado796@gmail.com>
2022-02-06 09:56:56 -0500

    ADD ckeleton impl

---
+++ lib/core/location/app/use_case/impl/location_use_case_impl.dart
@@ -0,0 +1,35 @@
+import 'package:jobits_pos_client_sales/core/location/app/models/location_model.dart';
+import 'package:jobits_pos_client_sales/core/location/app/use_case/location_use_case.dart';
+
+class location_use_case_impl implements location_use_case {
+  
+  @override
+  location_model choose_location(location_model model) {
+    // TODO: implement choose_location
+    throw UnimplementedError();
+  }
+
+  @override
+  location_model edit_location(location_model model) {
+    // TODO: implement edit_location
+    throw UnimplementedError();
+  }
+
+  @override
+  List<location_model> load_locations() {
+    // TODO: implement load_locations
+    throw UnimplementedError();
+  }
+
+  @override
+  location_model resolve_choosed_location() {
+    // TODO: implement resolve_choosed_location
+    throw UnimplementedError();
+  }
+
+  @override
+  List<location_model> save_locations(List<location_model> locations) {
+    // TODO: implement save_locations
+    throw UnimplementedError();
+  }
+}
---
+++ lib/core/location/repo/impl/location_repo_impl.dart
@@ -0,0 +1,47 @@
+import 'package:jobits_pos_client_sales/core/location/app/models/location_model.dart';
+import 'package:jobits_pos_client_sales/core/location/app/repo/location_repo.dart';
+import 'package:jobits_pos_client_sales/objectbox.g.dart';
+
+class location_repo_impl implements location_repo {
+
+
+
+
+  location_repo_impl() {
+    //final store = openStore();
+  //  final Box location_box = store.box<location_model>();
+  }
+
+  @override
+  location_model choose_location(location_model model) {
+    throw UnimplementedError();
+  }
+
+  @override
+  location_model edit_location(location_model model) {
+    // TODO: implement edit_location
+    throw UnimplementedError();
+  }
+
+  @override
+  List<location_model> load_locations() {
+    //List<location_model> loc = store.bo
+    // return preferences.get
+    throw UnimplementedError();
+
+  }
+
+  @override
+  location_model resolve_choosed_location() {
+    // TODO: implement resolve_choosed_location
+    throw UnimplementedError();
+  }
+
+  @override
+  List<location_model> save_locations(List<location_model> locations) {
+    // TODO: implement save_locations
+    throw UnimplementedError();
+  }
+
+
+}
