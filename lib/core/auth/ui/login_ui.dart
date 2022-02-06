e7bad0d68079fbe76181c8d1859316baf0ed517d
Sergio <8042sergi@gmail.com>
2022-02-06 14:27:34 -0500

    ADD loguin_ui

---
+++ lib/core/auth/ui/login_ui.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class login_ui extends StatelessWidget {
+  final TextEditingController textUser = TextEditingController();
+  final TextEditingController textPass = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size.width / 3;
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Autenticar',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                  )),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: size,
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(8.0)),
+                    ),
+                  ),
+                  controller: textUser,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: size,
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(8.0)),
+                    ),
+                  ),
+                  controller: textPass,
+                ),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Autenticar'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
