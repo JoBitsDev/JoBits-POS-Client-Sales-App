c72321b6f0624ac9f47ee0f82572ad3936b50205
Jorge Hurtado <jjhurtado796@gmail.com>
2022-02-06 00:05:30 -0500

    UPD id and entity

--- lib/core/location/app/models/location_model.dart
+++ lib/core/location/app/models/location_model.dart
@@ -1,13 +1,15 @@
-class location_model {
+import 'package:objectbox/objectbox.dart';
 
+@Entity()
+class location_model {
+  int id;
   String name;
-  String url;//ip+port
+  String url; //ip+port
   int data_base_id;
   int user_id;
   String user_name;
   String user_password;
 
-  location_model(
-       this.name, this.url, this.data_base_id, this.user_id,
+  location_model(this.id, this.name, this.url, this.data_base_id, this.user_id,
       this.user_name, this.user_password);
 }
