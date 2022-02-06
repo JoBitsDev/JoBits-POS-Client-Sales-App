6117de7018fc7b02d76e29cdfa80ebec4fc55b2f
Jorge Hurtado <jjhurtado796@gmail.com>
2022-02-06 14:22:22 -0500

    UPD Location Use case

--- lib/core/location/app/repo/location_repo.dart
+++ lib/core/location/app/repo/location_repo.dart
@@ -1,7 +1,6 @@
 import 'package:jobits_pos_client_sales/core/location/app/models/location_model.dart';
 
-abstract class LocationUseCase {
-
+abstract class LocationRepo {
   List<LocationModel> load_locations();
 
   List<LocationModel> save_locations(List<LocationModel> locations);
@@ -11,5 +10,4 @@ abstract class LocationUseCase {
   LocationModel choose_location(LocationModel model);
 
   LocationModel resolve_choosed_location();
-
 }
--- lib/core/location/repo/impl/location_repo_impl.dart
+++ lib/core/location/repo/impl/location_repo_impl.dart
@@ -1,8 +1,8 @@
 import 'package:jobits_pos_client_sales/core/location/app/models/location_model.dart';
-import 'package:jobits_pos_client_sales/core/location/repo/location_repo.dart';
-import 'package:jobits_pos_client_sales/objectbox.g.dart';
+import 'package:jobits_pos_client_sales/core/location/app/repo/location_repo.dart';
+//import 'package:jobits_pos_client_sales/objectbox.g.dart';
 
-class location_repo_impl implements location_repo {
+class location_repo_impl implements LocationRepo {
 
 
 
@@ -13,18 +13,18 @@ class location_repo_impl implements location_repo {
   }
 
   @override
-  location_model choose_location(location_model model) {
+  LocationModel choose_location(LocationModel model) {
     throw UnimplementedError();
   }
 
   @override
-  location_model edit_location(location_model model) {
+  LocationModel edit_location(LocationModel model) {
     // TODO: implement edit_location
     throw UnimplementedError();
   }
 
   @override
-  List<location_model> load_locations() {
+  List<LocationModel> load_locations() {
     //List<location_model> loc = store.bo
     // return preferences.get
     throw UnimplementedError();
@@ -32,13 +32,13 @@ class location_repo_impl implements location_repo {
   }
 
   @override
-  location_model resolve_choosed_location() {
+  LocationModel resolve_choosed_location() {
     // TODO: implement resolve_choosed_location
     throw UnimplementedError();
   }
 
   @override
-  List<location_model> save_locations(List<location_model> locations) {
+  List<LocationModel> save_locations(List<LocationModel> locations) {
     // TODO: implement save_locations
     throw UnimplementedError();
   }
