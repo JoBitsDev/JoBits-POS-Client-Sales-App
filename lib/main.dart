d3cd4bb8b954bcf8a4d2ab1cdccd44a38d0032cd
Cristhian Damian Rodriguez Aguila <cristhiandamianrodriguez@gmail.com>
2022-02-06 12:50:54 -0500

    upd main view

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:jobits_pos_client_sales/config/themes/theme_light.dart';
 import 'package:jobits_pos_client_sales/main_controller.dart';
 
 void main() => runApp(GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Home(),
       theme: theme_light.lightTheme,
     ));
@@ -14,26 +15,68 @@ class Home extends StatelessWidget {
     // Instantiate your class using Get.put() to make it available for all "child" routes there.
     final main_controller c = Get.put(main_controller());
     return Scaffold(
-        // Use Obx(()=> to update Text() whenever count is changed.
-        appBar: AppBar(title: Obx(() => Text("Clicks: ${c.count}"))),
-
-        // Replace the 8 lines Navigator.push by a simple Get.to(). You don't need context
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('POS Dpte'),
-              ElevatedButton(
-                child: Text("Go to Other"),
-                onPressed: () => Get.to(
-                  Other(),
-                ),
-              ),
-            ],
-          ),
+      // Use Obx(()=> to update Text() whenever count is changed.
+      appBar: AppBar(
+        title: Text(
+          "POS Dpte",
         ),
-        floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add), onPressed: c.increment));
+      ),
+
+      // Replace the 8 lines Navigator.push by a simple Get.to(). You don't need context
+      body: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'POS Dpte',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                ElevatedButton(
+                  child: Text("Iniciar sesión"),
+                  onPressed: () => Get.to(
+                    Other(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 25,
+            right: 10,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text('Hello world'),
+            ),
+          ),
+          Positioned(
+            bottom: 5,
+            right: 10,
+            child: Text(
+              'Version Number',
+            ),
+          ),
+          Positioned(
+            left: 10,
+            bottom: 10,
+            child: Text(
+              'Hurtado singao',
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
 
