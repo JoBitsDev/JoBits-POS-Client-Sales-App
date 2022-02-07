b0721066664984bd54df7868133500b387fbc020
Jorge Hurtado <jjhurtado796@gmail.com>
2022-02-06 19:13:55 -0500

    UPD location model

--- lib/core/location/repo/entity/location_entity.dart
+++ lib/core/location/repo/entity/location_entity.dart
@@ -1,7 +1,7 @@
 import 'package:objectbox/objectbox.dart';
 
 @Entity()
-class LocationModel {
+class LocationEntity{
   int id;
   String name;
   String url; //ip+port
@@ -10,6 +10,6 @@ class LocationModel {
   String user_name;
   String user_password;
 
-  LocationModel(this.id, this.name, this.url, this.data_base_id, this.user_id,
+  LocationEntity(this.id, this.name, this.url, this.data_base_id, this.user_id,
       this.user_name, this.user_password);
 }
