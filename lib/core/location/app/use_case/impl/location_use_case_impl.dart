c0db0029b9a432a60258cd1381d49d2fdb0366aa
Jorge Hurtado <jjhurtado796@gmail.com>
2022-02-06 20:47:27 -0500

    UPD implemented location UC

--- lib/core/location/app/use_case/impl/location_use_case_impl.dart
+++ lib/core/location/app/use_case/impl/location_use_case_impl.dart
@@ -1,35 +1,33 @@
+import 'package:get/get.dart';
 import 'package:jobits_pos_client_sales/core/location/app/model/location_model.dart';
+import 'package:jobits_pos_client_sales/core/location/app/repo/location_repo.dart';
 import 'package:jobits_pos_client_sales/core/location/app/use_case/location_use_case.dart';
 
 class LocationUseCaseImpl implements LocationUseCase {
-  
+  LocationRepo repo = Get.find();
+
   @override
   LocationModel choose_location(LocationModel model) {
-    // TODO: implement choose_location
-    throw UnimplementedError();
+    return repo.choose_location(model);
   }
 
   @override
   LocationModel edit_location(LocationModel model) {
-    // TODO: implement edit_location
-    throw UnimplementedError();
+    return repo.edit_location(model);
   }
 
   @override
   List<LocationModel> load_locations() {
-    // TODO: implement load_locations
-    throw UnimplementedError();
+    return repo.load_locations();
   }
 
   @override
   LocationModel resolve_choosed_location() {
-    // TODO: implement resolve_choosed_location
-    throw UnimplementedError();
+    return repo.resolve_choosed_location();
   }
 
   @override
   List<LocationModel> save_locations(List<LocationModel> locations) {
-    // TODO: implement save_locations
-    throw UnimplementedError();
+    return repo.save_locations(locations);
   }
 }
