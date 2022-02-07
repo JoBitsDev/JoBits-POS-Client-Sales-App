a6b8fe6169d645ebd2a3608014fd2148d81f37cf
Jorge Hurtado <jjhurtado796@gmail.com>
2022-02-06 20:47:05 -0500

    ADD ObjectBox config

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,1 +1,7 @@
 //This is where all our application constants will be present and this is different for each application.
+
+import 'package:jobits_pos_client_sales/utils/helpers/object_box_helper.dart';
+
+class AppConstants {
+  static late ObjectBox objectBox;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:jobits_pos_client_sales/config/themes/theme_light.dart';
+import 'package:jobits_pos_client_sales/constants/app_constants.dart';
 import 'package:jobits_pos_client_sales/constants/internationalization/spanish.dart';
 import 'package:jobits_pos_client_sales/core/welcome/welcome_ui.dart';
-import 'package:jobits_pos_client_sales/main_controller.dart';
+import 'package:jobits_pos_client_sales/utils/helpers/object_box_helper.dart';
 
-void main() => runApp(GetMaterialApp(
-      translations: Spanish(),
-      locale: Locale('es', 'ES'),
-      debugShowCheckedModeBanner: false,
-      home: WelcomeUI(),
-      theme: theme_light.lightTheme,
-    ));
 
+void main() {
+  initDataBase();
+  runApp(GetMaterialApp(
+    translations: Spanish(),
+    locale: Locale('es', 'ES'),
+    debugShowCheckedModeBanner: false,
+    home: WelcomeUI(),
+    theme: theme_light.lightTheme,
+  ));
+}
+
+void initDataBase() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  AppConstants.objectBox = await ObjectBox.create();
+}
