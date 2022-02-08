4e87be837edca7017f7981c58031933f33758e55
Jorge Hurtado <jjhurtado796@gmail.com>
2022-02-08 08:11:55 -0500

    ADD by default

--- lib/core/location/app/model/location_model.dart
+++ lib/core/location/app/model/location_model.dart
@@ -2,14 +2,16 @@ import 'package:objectbox/objectbox.dart';
 
 @Entity()
 class LocationModel {
-  int id;
-  String name;
-  String url; //ip+port
-  int data_base_id;
-  int user_id;
-  String user_name;
-  String user_password;
+  int id = -1;
+  String name = "Defecto";
+  String url = '0.0.0.0:0000'; //ip+port
+  int data_base_id = -1;
+  int user_id = -1;
+  String user_name = 'admin';
+  String user_password = 'admin';
 
   LocationModel(this.id, this.name, this.url, this.data_base_id, this.user_id,
       this.user_name, this.user_password);
+
+  LocationModel.byDefault();
 }
