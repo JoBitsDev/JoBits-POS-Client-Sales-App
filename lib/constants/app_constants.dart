429a6bb81f630b18a9446849ae389fff7807634b
Jorge Hurtado <jjhurtado796@gmail.com>
2022-02-09 16:02:50 -0500

    ADD shared preferences

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,7 +1,9 @@
 //This is where all our application constants will be present and this is different for each application.
 
-import 'package:jobits_pos_client_sales/utils/helpers/object_box_helper.dart';
+import 'package:jobits_pos_client_sales/utils/helpers/persistence_helper.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AppConstants {
   static late ObjectBox objectBox;
+  static late SharedPreferences sharedPreferences;
 }
