963dbf63d1904833956ad2f019cd68771e9b7ee4
Jorge Hurtado <jjhurtado796@gmail.com>
2022-02-09 16:03:27 -0500

    UPD main

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,14 @@ import 'package:get/get.dart';
 import 'package:jobits_pos_client_sales/config/themes/theme_light.dart';
 import 'package:jobits_pos_client_sales/constants/app_constants.dart';
 import 'package:jobits_pos_client_sales/constants/internationalization/spanish.dart';
+import 'package:jobits_pos_client_sales/core/location/module/location_module.dart';
 import 'package:jobits_pos_client_sales/core/welcome/welcome_ui.dart';
-import 'package:jobits_pos_client_sales/utils/helpers/object_box_helper.dart';
-
+import 'package:jobits_pos_client_sales/utils/helpers/persistence_helper.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   initDataBase();
+  initModules();
   runApp(GetMaterialApp(
     translations: Spanish(),
     locale: Locale('es', 'ES'),
@@ -18,7 +20,12 @@ void main() {
   ));
 }
 
+void initModules() async {
+  LocationCoreModule.init();
+}
+
 void initDataBase() async {
   WidgetsFlutterBinding.ensureInitialized();
   AppConstants.objectBox = await ObjectBox.create();
+  AppConstants.sharedPreferences = await SharedPreferences.getInstance();
 }
